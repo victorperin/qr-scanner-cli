176c3eafad6e55b4bd8efd207286e8804d312460
Leandro Nunes <dr1design@gmail.com>
2019-10-08 22:59:16 -0300

    feat: add prettier for checkstyle

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 #! /usr/bin/env node
-const Jimp = require('jimp');
-const fs = require('fs');
-const QrCode = require('qrcode-reader');
+const Jimp = require('jimp')
+const fs = require('fs')
+const QrCode = require('qrcode-reader')
 
-const buffer = fs.readFileSync(process.argv[2]);
+const buffer = fs.readFileSync(process.argv[2])
 Jimp.read(buffer, (err, image) => {
   if (err) {
-    console.error(err);
+    console.error(err)
     // TODO handle error
   }
-  const qr = new QrCode();
+  const qr = new QrCode()
   qr.callback = (callbackError, value) => {
     if (callbackError) {
-      console.error(callbackError);
+      console.error(callbackError)
       // TODO handle error
     }
-    console.log(value.result);
-  };
-  qr.decode(image.bitmap);
-});
+
+    console.log(value.result)
+  }
+  qr.decode(image.bitmap)
+})
