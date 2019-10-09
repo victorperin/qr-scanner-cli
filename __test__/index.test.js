21be52de83f7784dadef7d9925281464ee3ec565
Leandro Nunes <dr1design@gmail.com>
2019-10-09 01:11:40 -0300

    feat: add tests

---
+++ __test__/index.test.js
@@ -0,0 +1,56 @@
+const execa = require('execa')
+
+const ERROR = {
+  MISSING_PARAMS_FILE: '[WARNING] Missing argument file: node index.js <file>!',
+  FILE_NOT_FOUND: file => `[ERROR] File <${file}> not found!`,
+  PATTERN_NOT_FOUND: '[WARNING] No pattern could be found! Is there a QR-Code?',
+}
+
+test('Should read successfully the URL from QR-Code', async () => {
+  const img = '__test__/fixture/sample.jpg'
+  const { stdout } = await execa('node', ['index.js', img])
+
+  const result = stdout
+  const expected = 'https://github.com/victorperin/qr-scanner-cli'
+  expect(result).toEqual(expect.stringContaining(expected))
+})
+
+test('Should handle missing parameter <file>', async () => {
+  try {
+    await execa('node', ['index.js'])
+  } catch (err) {
+    const { failed, stderr } = err
+
+    const result = stderr
+    const expected = ERROR.MISSING_PARAMS_FILE
+    expect(failed).toBeTruthy()
+    expect(result).toEqual(expect.stringContaining(expected))
+  }
+})
+
+test('Should handle file not found', async () => {
+  const img = '404-notfound.jpg'
+  try {
+    await execa('node', ['index.js', img])
+  } catch (err) {
+    const { failed, stderr } = err
+
+    const result = stderr
+    const expected = ERROR.FILE_NOT_FOUND(img)
+    expect(failed).toBeTruthy()
+    expect(result).toEqual(expect.stringContaining(expected))
+  }
+})
+
+test('Should handle invalid file (no QR-Code)', async () => {
+  try {
+    await execa('node', ['index.js', '__test__/fixture/invalid.jpg'])
+  } catch (err) {
+    const { failed, stderr } = err
+
+    const result = stderr
+    const expected = ERROR.PATTERN_NOT_FOUND
+    expect(failed).toBeTruthy()
+    expect(result).toEqual(expect.stringContaining(expected))
+  }
+})
---
+++ index.js
@@ -0,0 +1,98 @@
+#! /usr/bin/env node
+const Jimp = require('jimp')
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const meow = require('meow')
+const QrCode = require('qrcode-reader')
+
+const cli = meow(
+  `
+    Usage
+      $ qrscanner <input file>
+
+    Options
+      --clean, -c  Clear output, just print the QR Code scan result
+      --version Show installed version
+      --help Show this help
+
+    Examples
+      $ qrscanner ./qrCode.jpg
+      ${chalk.green('╔══════════════════════════════════════════╗')}
+      ${chalk.green('║')}                                          ${chalk.green('║')}
+      ${chalk.green('║')}   This message is written in a QR Code   ${chalk.green('║')}
+      ${chalk.green('║')}                                          ${chalk.green('║')}
+      ${chalk.green('╚══════════════════════════════════════════╝')}
+
+      $ qrscanner ./qrCode.jpg --clear
+      This message is written in a QR Code
+`,
+  {
+    flags: {
+      clear: {
+        type: 'boolean',
+        alias: 'c',
+      },
+    },
+  },
+)
+
+const quit = (code = 1) => process.exit(code)
+
+function qrScannerCli(inputFile, { clear }) {
+  if (!inputFile) {
+    console.warn(`[WARNING] Missing argument file: node index.js <file>!`)
+    cli.showHelp(1)
+  }
+
+  if (!fs.existsSync(inputFile)) {
+    console.error(
+      boxen(`[ERROR] File <${inputFile}> not found!`, {
+        padding: 1,
+        borderStyle: 'double',
+        borderColor: 'red',
+      }),
+    )
+    quit(1)
+  }
+
+  const buffer = fs.readFileSync(inputFile)
+  Jimp.read(buffer, (err, image) => {
+    if (err) {
+      console.error(err)
+      // TODO handle error
+      return
+    }
+
+    const qr = new QrCode()
+    qr.callback = (callbackError, value) => {
+      if (callbackError) {
+        console.warn(
+          boxen('[WARNING] No pattern could be found! Is there a QR-Code?', {
+            padding: 1,
+            borderStyle: 'double',
+            borderColor: 'yellow',
+          }),
+        )
+        quit(1)
+      }
+
+      const scanResult = value.result
+      if (clear) {
+        console.log(scanResult)
+        return
+      }
+
+      console.log(
+        boxen(scanResult, {
+          padding: 1,
+          borderStyle: 'double',
+          borderColor: 'green',
+        }),
+      )
+    }
+    qr.decode(image.bitmap)
+  })
+}
+
+qrScannerCli(cli.input[0], cli.flags)
