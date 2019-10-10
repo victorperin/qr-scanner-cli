ebb91bbe28a4018d61baa6f106610674f11b52f5
wjbaker101 <willbaker10198@gmail.com>
2019-10-10 19:34:53 +0100

    Add functionality for a add-to-clipboard parameter

--- src/scanFromFile.js
+++ src/scanFromFile.js
@@ -1,4 +1,5 @@
 const Jimp = require('jimp')
+const clipboardy = require('clipboardy')
 
 const { readFile } = require('./infrastructure/fs')
 const { greenBox } = require('./infrastructure/boxen')
@@ -8,13 +9,33 @@ const logWithGreenBox = text => console.log(greenBox(text))
 
 const extractBitmap = ({ bitmap }) => bitmap
 
+const doFlagClear = (text, flags) => {
+  if (flags.clear) {
+    console.log(text)
+  }
+  else {
+    logWithGreenBox(text)
+  }
+
+  return text
+}
+
+const doFlagClipboard = (text, flags) => {
+  if (flags.clipboard) {
+    clipboardy.writeSync(text)
+  }
+
+  return text
+}
+
 const scanFromFile = (filePath, flags) =>
   Promise.resolve(filePath)
     .then(readFile)
     .then(Jimp.read)
     .then(extractBitmap)
     .then(readQR)
-    .then(flags.clear ? console.log : logWithGreenBox)
+    .then(t => doFlagClear(t, flags))
+    .then(t => doFlagClipboard(t, flags))
     .catch(console.error)
 
 module.exports = scanFromFile
