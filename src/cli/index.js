49a8ad67d1c1b92ef5ff10bb3fedd1a9633d1364
Victor Perin <me@victorperin.ninja>
2019-10-10 11:29:18 -0300

    fix(linter): Parsing error: 'return' outside of function

--- src/cli/index.js
+++ src/cli/index.js
@@ -6,13 +6,16 @@ const scanFromFile = require('../scanFromFile')
 const helpText = require('./helpText')
 const flags = require('./flags')
 
-
 const options = { flags }
 const cli = meow(helpText, options)
 
-if(!cli.input.length) {
-  console.warn(`[WARNING] Missing argument file: node index.js <file>!`)
-  return cli.showHelp(1)
+const execute = () => {
+  if (!cli.input.length) {
+    console.warn(`[WARNING] Missing argument file: node index.js <file>!`)
+    return cli.showHelp(1)
+  }
+
+  return scanFromFile(cli.input[0], cli.flags)
 }
 
-scanFromFile(cli.input[0], cli.flags)
+execute()
