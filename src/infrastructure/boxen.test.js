7d1b59790e1592787ad520502e72944c22fc7421
Victor Perin <me@victorperin.ninja>
2019-10-18 20:51:04 -0300

    test(infrastructure/boxen): replace snapshot testing

--- src/infrastructure/boxen.test.js
+++ src/infrastructure/boxen.test.js
@@ -1,5 +1,16 @@
+const { stripIndent } = require('common-tags')
+
 const { greenBox } = require('./boxen')
 
 test('Should place a green box around the input', async () => {
-  expect(greenBox('sample input')).toMatchSnapshot()
+  const result = greenBox('sample input')
+  const expected = stripIndent`
+    [32m╔══════════════════╗[39m
+    [32m║[39m                  [32m║[39m
+    [32m║[39m   sample input   [32m║[39m
+    [32m║[39m                  [32m║[39m
+    [32m╚══════════════════╝[39m
+  `
+
+  expect(result).toEqual(expected)
 })
