6b2e433649c7a449477cc4697c22dfba20747bac
Victor Perin <me@victorperin.ninja>
2019-10-19 14:48:35 -0300

    refactor(cli): split cli to simpify unit testing

---
+++ src/cli/execution.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+const meow = require('meow')
+
+const scanFromFile = require('../scanFromFile')
+
+const helpText = require('./helpText')
+const flags = require('./flags')
+
+const options = { flags }
+
+const execution = () => {
+  const cli = meow(helpText, options)
+
+  if (!cli.input.length) {
+    console.warn(`[WARNING] Missing argument file: node index.js <file>!`)
+    return cli.showHelp(1)
+  }
+
+  return scanFromFile(cli.input[0], cli.flags)
+}
+
+module.exports = execution
--- src/cli/index.js
+++ src/cli/index.js
@@ -1,21 +1,4 @@
 #! /usr/bin/env node
-const meow = require('meow')
+const execution = require('./execution')
 
-const scanFromFile = require('../scanFromFile')
-
-const helpText = require('./helpText')
-const flags = require('./flags')
-
-const options = { flags }
-const cli = meow(helpText, options)
-
-const execute = () => {
-  if (!cli.input.length) {
-    console.warn(`[WARNING] Missing argument file: node index.js <file>!`)
-    return cli.showHelp(1)
-  }
-
-  return scanFromFile(cli.input[0], cli.flags)
-}
-
-execute()
+execution()
