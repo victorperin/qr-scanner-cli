56722d0a7184eeef31bd706ad3d2901c429c8603
Victor Perin <me@victorperin.ninja>
2019-10-19 01:59:03 -0300

    test(infrastructure/qrcode-reader): should copy to clipboard if has flag

--- src/scanFromFile.test.js
+++ src/scanFromFile.test.js
@@ -1,4 +1,5 @@
 const Jimp = require('jimp')
+const clipboardy = require('clipboardy')
 const fs = require('./infrastructure/fs')
 const qrReader = require('./infrastructure/qrcode-reader')
 const boxen = require('./infrastructure/boxen')
@@ -6,6 +7,7 @@ const boxen = require('./infrastructure/boxen')
 const scanFromFile = require('./scanFromFile')
 
 jest.mock('jimp')
+jest.mock('clipboardy')
 jest.mock('./infrastructure/fs')
 jest.mock('./infrastructure/qrcode-reader')
 jest.mock('./infrastructure/boxen')
@@ -15,6 +17,7 @@ beforeEach(() => {
   Jimp.read.mockResolvedValue({ bitmap: 'FAKE BITMAP' })
   qrReader.mockResolvedValue('FAKE QR CONTENT')
   boxen.greenBox.mockReturnValue('FAKE BOX')
+  clipboardy.writeSync.mockResolvedValue('FAKE CLIPBOARD')
 })
 
 afterEach(jest.restoreAllMocks)
@@ -29,6 +32,8 @@ test('should pass on happy path', async () => {
   expect(qrReader).toBeCalledWith('FAKE BITMAP')
   expect(boxen.greenBox).toBeCalledWith('FAKE QR CONTENT')
   expect(console.log).toBeCalledWith('FAKE BOX')
+
+  expect(clipboardy.writeSync).not.toBeCalledWith('FAKE QR CONTENT')
 })
 
 test('should console.error if readFile fails', async () => {
@@ -39,4 +44,18 @@ test('should console.error if readFile fails', async () => {
 
   expect(fs.readFile).toBeCalledWith('FAKE PATH')
   expect(console.error).toBeCalledWith('FAKE ERROR')
+})
+
+test('should copy to clipboard if flag is present', async () => {
+  jest.spyOn(global.console, 'log').mockReturnValue()
+
+  await scanFromFile('FAKE PATH', { clipboard: true })
+
+  expect(fs.readFile).toBeCalledWith('FAKE PATH')
+  expect(Jimp.read).toBeCalledWith('FAKE FILE CONTENT')
+  expect(qrReader).toBeCalledWith('FAKE BITMAP')
+  expect(boxen.greenBox).toBeCalledWith('FAKE QR CONTENT')
+
+  expect(clipboardy.writeSync).toBeCalledWith('FAKE QR CONTENT')
+  expect(console.log).toBeCalledWith('FAKE BOX')
 })
