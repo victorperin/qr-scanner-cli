d9a17c4940df2a319550230075fcb444bd4108ba
BrOrlandi <brorlandi@gmail.com>
2019-10-21 14:12:38 -0300

    refactor: isolate CLI code to execution file instead of scanFromFile

--- src/cli/execution.js
+++ src/cli/execution.js
@@ -1,11 +1,28 @@
 const meow = require('meow')
+const clipboardy = require('clipboardy')
 
 const scanFromFile = require('../scanFromFile')
+
+const { greenBox } = require('../infrastructure/boxen')
 
 const helpText = require('./helpText')
 const flags = require('./flags')
 
 const options = { flags }
+
+const outputText = (text, cliFlags) => {
+  const output = cliFlags.clear ? text : greenBox(text)
+
+  console.log(output)
+}
+
+const doFlagClipboard = (text, cliFlags) => {
+  if (cliFlags.clipboard) {
+    clipboardy.writeSync(text)
+  }
+
+  return text
+}
 
 const execution = () => {
   const cli = meow(helpText, options)
@@ -15,7 +32,10 @@ const execution = () => {
     return cli.showHelp(1)
   }
 
-  return scanFromFile(cli.input[0], cli.flags)
+  return scanFromFile(cli.input[0])
+    .then(t => doFlagClipboard(t, cli.flags))
+    .then(t => outputText(t, cli.flags))
+    .catch(console.error)
 }
 
 module.exports = execution
--- src/scanFromFile.js
+++ src/scanFromFile.js
@@ -1,34 +1,15 @@
 const Jimp = require('jimp')
-const clipboardy = require('clipboardy')
 
 const { readFile } = require('./infrastructure/fs')
-const { greenBox } = require('./infrastructure/boxen')
 const readQR = require('./infrastructure/qrcode-reader')
 
 const extractBitmap = ({ bitmap }) => bitmap
 
-const outputText = (text, flags) => {
-  const output = flags.clear ? text : greenBox(text)
-
-  console.log(output)
-}
-
-const doFlagClipboard = (text, flags) => {
-  if (flags.clipboard) {
-    clipboardy.writeSync(text)
-  }
-
-  return text
-}
-
-const scanFromFile = (filePath, flags) =>
+const scanFromFile = filePath =>
   Promise.resolve(filePath)
     .then(readFile)
     .then(Jimp.read)
     .then(extractBitmap)
     .then(readQR)
-    .then(t => doFlagClipboard(t, flags))
-    .then(t => outputText(t, flags))
-    .catch(console.error)
 
 module.exports = scanFromFile
