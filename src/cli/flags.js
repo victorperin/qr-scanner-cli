786fc56b3435978311ece70eebad1799ac005b9f
Chris Young <xtops@xtopsmbp.local>
2019-10-23 23:15:49 -0700

    fix: #64 Generate help text from flags file

--- src/cli/flags.js
+++ src/cli/flags.js
@@ -1,6 +1,14 @@
 const flags = {
-  clear: { type: 'boolean', alias: 'c' },
-  clipboard: { type: 'boolean', alias: 'p' },
+  clear: {
+    type: 'boolean',
+    alias: 'c',
+    description: 'Clear output, just print the QR Code scan result',
+  },
+  clipboard: {
+    type: 'boolean',
+    alias: 'p',
+    description: 'Copy the qr code value to your clipboard',
+  },
 }
 
 module.exports = flags
