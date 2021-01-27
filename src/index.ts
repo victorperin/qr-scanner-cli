c782de48d73b931ef1102be507931cc6968bc4fd
Victor Perin <me@victorperin.ninja>
2021-01-27 00:26:48 -0300

    feat(pipelines): add function comments for programatic mode

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { scanFromFile } from './pipelines/scanFromFile'
+export { Flags } from './cli/flags'
--- src/pipelines/scanFromFile.ts
+++ src/pipelines/scanFromFile.ts
@@ -4,12 +4,22 @@ import readQR from '../infrastructure/qrcode-reader'
 import { outputText, doFlagClipboard, doOpen } from '../handlers/flags'
 import { Flags } from '../cli/flags'
 
-export const scanFromFile = (filePath: string, flags: Flags = {}): Promise<string> =>
+/**
+ * This function reads a file, get it's bitmap, searches and read a qrcode from it.
+ * ```
+ * const value = await scanFromFile('./image.jpg')
+ * ```
+ *
+ * @param filePath A file path for an image file containing a QR Code
+ * @param flags
+ * @returns Value read from the QR Code inside the image
+ */
+export const scanFromFile = (filePath: string, flags?: Flags): Promise<string> =>
   Promise.resolve(filePath)
     .then(getBitmap)
     .then(readQR)
-    .then(doFlagClipboard(flags))
-    .then(doOpen(flags))
+    .then(doFlagClipboard(flags || {}))
+    .then(doOpen(flags || {}))
     .catch(errorHandlers.scanFromFile(filePath))
 
 export const scanFromFileOnCli = (filePath: string, flags: Flags): Promise<void> =>
