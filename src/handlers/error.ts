653d5fb845d5e485b7216563ee5400c1101a61f6
Victor Perin <me@victorperin.ninja>
2021-01-30 10:24:04 -0300

    feat(programatic): add scanFromBitmap function

--- src/handlers/error.ts
+++ src/handlers/error.ts
@@ -1,15 +1,18 @@
-export const scanFromFile = (filePath: string) => (error: Error | string): never => {
+export const scanFromFile = (filePath: string) => (error: Error): never => {
+  if (error.message.includes('no such file or directory'))
+    throw new Error(`[ERROR] File <${filePath}> not found!`)
+
+  throw error
+}
+
+export const scanFromBitmap = (error: Error | string): never => {
   if (typeof error === 'string' && error.includes('0 patterns found'))
     throw new Error('[WARNING] No pattern could be found! Is there a QR-Code?')
 
-  const errorOnly = error as Error
-
-  if (errorOnly.message.includes('no such file or directory'))
-    throw new Error(`[ERROR] File <${filePath}> not found!`)
-
-  throw errorOnly
+  throw error
 }
 
 export default {
   scanFromFile,
+  scanFromBitmap,
 }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+// Functions
 export { scanFromFile } from './pipelines/scanFromFile'
+export { scanFromBitmap } from './pipelines/scanFromBitmap'
+
+// Types
 export { Flags } from './cli/flags'
+export { Bitmap } from '@jimp/core'
