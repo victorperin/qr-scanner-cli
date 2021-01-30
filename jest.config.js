a268a300c7d124111934cb79fe89774ba5b40c66
Victor Perin <me@victorperin.ninja>
2021-01-30 11:30:25 -0300

    test(jest): add jest-extended

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   // collectCoverageFrom: ['src/**'],
   // coverageReporters: ['lcov', 'text'],
   // coverageDirectory: 'coverage',
+  setupFilesAfterEnv: ['./tests/setup.ts'],
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.test.ts', '<rootDir>/src/**/*.test.ts'],
   modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/coverage/', '<rootDir>/.nyc_output/'],
