ca283583ddec68c76b26034117450d80cab5bcde
Victor Perin <me@victorperin.ninja>
2021-01-30 11:31:22 -0300

    test(pipelines): add tests to scanFromBitmap

---
+++ src/pipelines/scanFromBitmap.test.ts
@@ -0,0 +1,66 @@
+import { scanFromBitmap } from './scanFromBitmap'
+import readQR from '../infrastructure/qrcode-reader'
+import { mocked } from 'ts-jest/utils'
+import { Bitmap } from '@jimp/core'
+import { createMock } from 'ts-auto-mock'
+import doFlags, { doFlagClipboard } from '../handlers/flags'
+import errorHandlers from '../handlers/error'
+
+jest.mock('../infrastructure/qrcode-reader')
+jest.mock('../handlers/flags')
+jest.mock('../handlers/error')
+const readQRMock = mocked(readQR)
+const doFlagsMock = mocked(doFlags, true)
+const errorHandlerMock = mocked(errorHandlers.scanFromBitmap)
+
+readQRMock.mockResolvedValue('readQR fake result')
+doFlagsMock.doFlagClipboard.mockImplementation(() => jest.fn((result) => result))
+doFlagsMock.doOpen.mockImplementation(() => jest.fn((result) => result))
+
+const fileBitmap: Bitmap = createMock<Bitmap>()
+
+describe('scanFromBitmap', () => {
+  it('should readQR', async () => {
+    await scanFromBitmap(fileBitmap)
+
+    expect(readQRMock).toBeCalledTimes(1)
+    expect(readQRMock).toBeCalledWith(fileBitmap)
+  })
+
+  it('should check for flag clipboard', async () => {
+    const flags = { clipboard: true }
+    await scanFromBitmap(fileBitmap, flags)
+
+    expect(doFlagsMock.doFlagClipboard).toBeCalledTimes(1)
+    expect(doFlagsMock.doFlagClipboard).toBeCalledWith(flags)
+    expect(doFlagsMock.doFlagClipboard.mock.results[0].value).toBeCalledWith('readQR fake result')
+    expect(doFlagsMock.doFlagClipboard.mock.results[0].value).toHaveBeenCalledAfter(readQRMock)
+  })
+
+  it('should check for flag open', async () => {
+    const flags = { clipboard: true }
+    await scanFromBitmap(fileBitmap, flags)
+
+    expect(doFlagsMock.doOpen).toBeCalledTimes(1)
+    expect(doFlagsMock.doOpen).toBeCalledWith(flags)
+    expect(doFlagsMock.doOpen.mock.results[0].value).toBeCalledWith('readQR fake result')
+    expect(doFlagsMock.doFlagClipboard.mock.results[0].value).toHaveBeenCalledAfter(readQRMock)
+  })
+
+  describe('errors', () => {
+    it('should should not execute errorHandle if no error occours', async () => {
+      await scanFromBitmap(fileBitmap)
+
+      expect(errorHandlerMock).not.toBeCalled()
+    })
+
+    it('should handle errors with errorHandlers', async () => {
+      const expectedError = new Error()
+      readQRMock.mockRejectedValueOnce(expectedError)
+      await scanFromBitmap(fileBitmap)
+
+      expect(errorHandlerMock).toBeCalledTimes(1)
+      expect(errorHandlerMock).toBeCalledWith(expectedError)
+    })
+  })
+})
