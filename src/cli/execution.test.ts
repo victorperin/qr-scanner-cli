3bacadd28ef506c6b549e7d5b467e5061dca35c5
Victor Perin <me@victorperin.ninja>
2021-08-05 19:36:24 -0300

    test(cli/execution): survived mutations #1 #3 #4 #5 #7 #8 #19 #20 #21 #22 #24 #25 #26 #27 #30 #31

--- src/cli/execution.test.ts
+++ src/cli/execution.test.ts
@@ -61,3 +61,55 @@ test('should warn if no argument passed', async () => {
   expect(yargsMocked.exit).toBeCalledWith(1, new Error('missing file path'))
   expect(console.warn).toBeCalledWith('[WARNING] Missing argument file: qrscanner <file>!')
 })
+
+test('should provide examples and options', async () => {
+  await execution([])
+
+  yargsMocked.example.mock.calls
+
+  expect(yargsMocked.example.mock.calls[0]).toMatchInlineSnapshot(`
+Array [
+  Array [
+    Array [
+      "qrscanner ./qrCode.jpg",
+      "[32m[39m
+[32m   ╔══════════════════════════════════════════╗[39m
+   [32m║[39m                                          [32m║[39m
+   [32m║[39m   This message is written in a QR Code   [32m║[39m
+   [32m║[39m                                          [32m║[39m
+[32m   ╚══════════════════════════════════════════╝[39m
+[32m[39m",
+    ],
+    Array [
+      "qrscanner ./qrCode.jpg --clear",
+      "
+This message is written in a QR Code",
+    ],
+  ],
+]
+`)
+  expect(yargsMocked.options.mock.calls[0]).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "clear": Object {
+      "alias": "c",
+      "boolean": true,
+      "default": false,
+      "description": "Clear output, just print the QR Code scan result",
+    },
+    "clipboard": Object {
+      "alias": "p",
+      "boolean": true,
+      "default": false,
+      "description": "Copy the qr code value to your clipboard",
+    },
+    "open": Object {
+      "alias": "o",
+      "boolean": true,
+      "default": false,
+      "description": "Open the qr code value in any browser or program if support it",
+    },
+  },
+]
+`)
+})
